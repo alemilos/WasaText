8ff40daac362bbd151aff1a89c83cd06b147916f
Alessandro Milos <alessandro.milos@raxarsystems.com>
2025-01-10 16:35:01 +0100

    using hash history

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -1,11 +1,11 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHashHistory } from "vue-router";
 
 // Pages
 import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
-	history: createWebHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory(import.meta.env.BASE_URL),
 	routes: [
 		{ path: "/", component: HomeView },
 		{ path: "/login", component: LoginView },
