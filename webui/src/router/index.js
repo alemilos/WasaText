7f9b66daed2fcc4af44bb3d2c61783bd68bd1e01
Alessandro Milos <alessandro.milos@raxarsystems.com>
2025-01-10 16:39:40 +0100

    changed to '/' route instead of import.meta...

--- webui/src/router/index.js
+++ webui/src/router/index.js
@@ -5,7 +5,7 @@ import HomeView from "../views/HomeView.vue";
 import LoginView from "../views/LoginView.vue";
 
 const router = createRouter({
-	history: createWebHashHistory(import.meta.env.BASE_URL),
+	history: createWebHashHistory("/"),
 	routes: [
 		{ path: "/", component: HomeView },
 		{ path: "/login", component: LoginView },
