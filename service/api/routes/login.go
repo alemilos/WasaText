c16f14b16163c8efd704f9b7908dc1b929f4a5c0
Alessandro Milos <alessandro@MacBook-Air-di-Alessandro.local>
2025-09-29 11:08:40 +0200

    openAPI on track

--- service/api/routes/login.go
+++ service/api/routes/login.go
@@ -2,13 +2,11 @@ package api
 
 import (
 	"net/http"
-
 	"github.com/julienschmidt/httprouter"
 )
 
 // login endpoint handler
 func (rt *_router) login(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
-	// Implement your login logic here
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte("Login successful"))
 }
---
+++ service/api/routes/setMyUserName.go
@@ -0,0 +1,9 @@
+package api
+
+import (
+	"net/http"
+	"github.com/julienschmidt/httprouter"
+)
+
+func (rt *_router) setMyUsername(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+}
