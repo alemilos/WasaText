eaff18545558cfc30cc89f0fd42a21c6d841dcd1
Alessandro Milos <alessandro@MacBook-Air-di-Alessandro.local>
2025-10-21 23:06:02 +0200

    fix format & more

--- service/api/constants/constants.go
+++ service/api/constants/constants.go
@@ -4,7 +4,7 @@ const (
 	MAX_UPLOAD_SIZE    = 2 * 1024 * 1024 // 2Mb
 	PROFILE_PHOTO_PATH = "/profiles"
 	GROUP_PHOTO_PATH   = "/groups"
-	MESSAGE_PHOTO_PATH   = "/messages"
+	MESSAGE_PHOTO_PATH = "/messages"
 )
 
 // conversation types
